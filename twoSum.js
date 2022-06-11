924831d877d1444872e6c5294a96a46a9b69e761
Asad Sheikh <asad@Asads-MacBook-Pro.local>
2022-06-10 22:12:52 -0400

    [edit] changed twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,6 @@
-// Time Complexity
-// O(n)
-
-// Space Complexity
-// O(1)
+// Approach 1
+// Time Complexity | O(n)
+// Space Complexity | O(1)
 
 function twoNumberSum(array, targetSum) {
   let output = [];
@@ -22,6 +20,26 @@ function twoNumberSum(array, targetSum) {
 }
 
 
+// Approach 2
+// Time Complexity: O(nlog(n))
+// Space Complexity: O(1) space
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) return [array[left], array[right]];
+    else if (currentSum < targetSum) left++;
+    else if (currentSum > targetSum) right--;
+  }
+  return [];
+}
+
+
+
+
 console.log(
   twoNumberSum([3,5, -4, 8, 11, 1, -1 , 6], 10)
 );
+
