3dc7f75223fe30674e1dcbc9c49128aa6c8ee18b
Asad Sheikh <asad@Asads-MacBook-Pro.local>
2022-07-16 20:10:48 -0400

    [add] permutations

---
+++ backtracking/permutations.js
@@ -0,0 +1,33 @@
+function permutations(nums) {
+  const results = [];
+  const temp = [];
+  const set = new Set();
+
+  const backtrack = () => {
+
+    if (temp.length === nums.length) {
+      results.push([...temp]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++){
+      let el = nums[i];
+      if (set.has(el)) continue;
+      else {
+        set.add(el);
+        temp.push(el);
+        backtrack(i + 1);
+        temp.pop();
+        set.delete(el);
+      }
+    }
+  }
+
+  backtrack();
+  return results;
+}
+
+console.log(
+  'permutations',
+  permutations(['A','B', 'C'])
+);
